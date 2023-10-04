65c5c6c24cba2c700df1e989ff59c8e19aaaa1f7
julien <julien.sanchez74@orange.fr>
2023-10-04 16:29:18 +0100

    fix : mistakes on roles verification in the treatment controller

--- src/treatment/treatment.controller.ts
+++ src/treatment/treatment.controller.ts
@@ -1,20 +1,21 @@
 import { ApiTags } from "@nestjs/swagger";
-import { TreatmentService } from './treatment.service';
-import { Body, Controller, UseGuards, Get, Param} from "@nestjs/common";
+import { TreatmentService } from "./treatment.service";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { HasRoles } from "../auth/has-roles.decorator";
 import { Role } from "../auth/interface/role.enum";
 import { AuthGuard } from "@nestjs/passport";
+import { RolesGuard } from "../auth/roles.guard";
 
 @ApiTags("Treatment")
-@Controller('treatment')
+@Controller("treatment")
 export class TreatmentController {
-    constructor(private treatmentService: TreatmentService) {
-    }
+  constructor(private treatmentService: TreatmentService) {
+  }
 
-    @Get(":id")
-    @UseGuards(AuthGuard("jwt"))
-    @HasRoles(Role.Admin)
-    async findOneById(@Param("id") id: number) {
+  @Get(":id")
+  @HasRoles(Role.Admin)
+  @UseGuards(AuthGuard("jwt"), RolesGuard)
+  async findOneById(@Param("id") id: number) {
     try {
       return await this.treatmentService.findOneById(id);
     } catch (error) {
@@ -22,13 +23,15 @@ export class TreatmentController {
     }
   }
 
-    @Get()
-    @UseGuards(AuthGuard("jwt"))
-    async showAllTreatments() {
-        try {
-        return await this.treatmentService.showAllTreatments();
-        } catch (error) {
-        throw error;
-        }
+  @Get()
+  @UseGuards(AuthGuard("jwt"))
+  @HasRoles(Role.Admin)
+  @UseGuards(AuthGuard("jwt"), RolesGuard)
+  async showAllTreatments() {
+    try {
+      return await this.treatmentService.showAllTreatments();
+    } catch (error) {
+      throw error;
     }
+  }
 }
